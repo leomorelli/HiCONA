541076fcd5f7782e237f0fd744a60a9997418cb6
Stefano Cretti <stefanocretti99@gmail.com>
2024-08-26 12:38:12 +0200

    cleanup: removed old type conversion functions
    Removed type conversion functions made obsolete from the migrations to polars.

--- hicona/_ops/dataf.py
+++ hicona/_ops/dataf.py
@@ -5,41 +5,6 @@ import pandas as pd
 import scipy as sp
 import polars as pl
 
-# TODO: somehow add category modality
-
-
-def from_df_to_sarrays(data: pd.DataFrame):
-    """Return each column of a dataframe as a numpy structured array.
-
-    Transform the columns of a dataframe into numpy structured array and
-    define the numpy datatype most appropriate for storage in HDF5 (especially
-    minimum required string fixed length for categorical annotations).
-    """
-
-    # TODO: Currently only discriminating string/non string, improve
-
-    dtypes = data.dtypes
-    for col_name, dtype in zip(data, dtypes):
-        if dtype == "object":
-            str_len = int(data[col_name].str.len().max())
-            str_len = str_len if str_len > 3 else 3
-            dtype = np.dtype(f"S{str_len}")
-
-        new_col = np.zeros(len(data), dtype)
-        new_col[:] = data[col_name].values
-
-        yield (col_name, new_col, dtype)
-
-
-def pd_to_h5_dtype(pd_dtype: str):
-    """Placeholder"""
-
-    h5_dtype = pd_dtype
-    if pd_dtype == "object":
-        h5_dtype = np.dtype("S10")
-
-    return h5_dtype
-
 
 def pd2gt_dtype(pd_dtype: str):
     """Convert pandas-like datatypes to graph-tools datatypes"""
