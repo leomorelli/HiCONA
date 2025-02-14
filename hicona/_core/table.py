a3b23d72dccdf6a7e5a68832ee17f0984b3cd8a6
Stefano Cretti <stefanocretti99@gmail.com>
2025-02-14 15:51:09 +0100

    qol: add copy as explicit method of table classes
    BinTable and PixelTable now also have an explicit copy method (which simply returns the output of the dunder method.

--- hicona/_core/table.py
+++ hicona/_core/table.py
@@ -140,6 +140,20 @@ class BinTable(Table):
     def __copy__(self) -> "BinTable":
         return BinTable(bins=self.get_dataframe(), store_size=self.store_size)
 
+    def copy(self) -> "BinTable":
+        """Returns a deep copy of itself.
+
+        A new temporary copy of all the data present in the table is created and the
+        handle to it is returned.
+
+        Returns
+        -------
+        BinTable
+            A deep copy of this object.
+        """
+
+        return self.__copy__()
+
     @property
     def bin_size(self) -> int:
         """Return the size of the bins in base pairs (resolution).
@@ -449,6 +463,20 @@ class PixelTable(Table):
             bins=self.bins.get_dataframe(),
             store_size=self.store_size,
         )
+
+    def copy(self) -> "PixelTable":
+        """Returns a deep copy of itself.
+
+        A new temporary copy of all the data present in the table is created and the
+        handle to it is returned.
+
+        Returns
+        -------
+        PixelTable
+            A deep copy of this object.
+        """
+
+        return self.__copy__()
 
     @property
     def bins(self) -> BinTable:
