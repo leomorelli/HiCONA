c06edbce12b59d336bd7624db796bb94f956fa3d
Stefano Cretti <stefanocretti99@gmail.com>
2026-06-07 13:26:10 +0200

    install: updated versioning system
    Updated versioning system to match the one used by Cooler

--- hicona/_version.py
+++ hicona/_version.py
@@ -1,1 +1,6 @@
-__version__ = "0.0.0.alpha"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("hicona")
+except PackageNotFoundError:
+    __version__ = "unknown"
